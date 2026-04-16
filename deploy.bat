d628b842b69bae115128cf918e9ddc6510f5fae9
Guharo <rodrigo_9510@hotmail.com>
2026-04-16 16:55:52 -0500

    Update deploy.bat

--- deploy.bat
+++ deploy.bat
@@ -1,8 +1,14 @@
 @echo off
+:: Cambia esto por la ruta real donde está tu carpeta de proyecto
+cd /d "D:\ProyectosSoftware\OnpeScrapper\OnpeScrapper"
+
 echo Ejecutando Scraper...
 dotnet run --project OnpeScrapper.csproj
+
 echo Subiendo cambios a GitHub...
 git add index.html
-git commit -m "Actualizacion de datos: %date% %time%"
+git commit -m "Actualizacion automatica: %date% %time%"
 git push origin main
+
 echo ¡Listo!
+pause
